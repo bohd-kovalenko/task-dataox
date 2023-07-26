c19201027bdd3c571448aa8accacee499889e7b3
CoolTrol <bohdan1234yunior@gmail.com>
2023-07-26 18:10:46 +0300

    Initial commit

---
+++ src/main/java/com/testtaskdataox/TestTaskDataoxApplication.java
@@ -0,0 +1,13 @@
+package com.testtaskdataox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskDataoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskDataoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testtaskdataox/TestTaskDataoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtaskdataox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskDataoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
